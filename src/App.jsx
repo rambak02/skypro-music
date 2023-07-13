aac5115f8887220ea383e11d2c8550d569d87edd
rambak02 <rambak02@mail.ru>
2023-07-13 16:47:05 +0700

    preparing to work with react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="Text">
+        <p>
+          Здесь скоро будет Skypro.MusicS
+        </p>
+    </div>
+  );
+}
+
+export default App;
